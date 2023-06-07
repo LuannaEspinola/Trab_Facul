5036c1023769be423630bab15efa40ef690b7243
Luanna <luannaespinola@hotmail.com>
2023-06-07 17:02:02 -0300

    first commit

---
+++ script/scrpit.js
@@ -0,0 +1,8 @@
+/*function piscar() {
+  var elemento = document.getElementById("piscar");
+  setInterval(function() {
+    elemento.style.visibility = (elemento.style.visibility == "hidden" ? "visible" : "hidden");
+  }, 500);
+}
+
+piscar();*/
